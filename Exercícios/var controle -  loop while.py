80206bac9efdb45b9dbf92128bd7b343901cbaae
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2021-08-18 21:29:24 -0300

    Var Controle - Loop While
    Variável de controle no loop while.

---
+++ Exercícios/var controle -  loop while.py
@@ -0,0 +1,7 @@
+# inicialização
+fim = 5        # número de iterações
+cont = 0        # variável de controle
+
+while cont < fim: # o loop irá ser feito *5* vezes
+    print('Iteracao numero: ', cont) # faça alguma coisa, nesse caso, apenas imprima cont
+    cont = cont + 1   # variável de controle precisa ser atualizada para garantir o fim do while
