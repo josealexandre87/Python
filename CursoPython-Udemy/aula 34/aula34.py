9f0a5a902a38b09249cd1ecf28268ff62f1ec6d5
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-01-31 16:53:04 -0300

    update

--- CursoPython-Udemy/aula 34/aula34.py
+++ CursoPython-Udemy/aula 34/aula34.py
@@ -13,3 +13,14 @@ if divide:
     print(f'{divide:.2f}') # se não houvesse um eslse, o resultado seria "NONE".
 else:
     print('Conta inválida! Defina um número para n2 diferente de zero(0)')
+
+
+######################
+
+def dumb():
+    return ('Luiz', 'Otávio')
+
+var = dumb() # uma variável pode receber uma função e torna-se um objeto do tipo'Function'.
+
+print(var[1], type(var), id(dumb()), id(var)) #pelo id nota-se que os dois tem o mesmo endereço na memória.
+# (saída do print>>>>) Otávio <class 'tuple'> 2324159792064 2324159792064
