0ec4dc7a0ba85c20e95be0432f1f84c4e3ecfa96
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-01-31 16:36:33 -0300

    commit test

--- CursoPython-Udemy/aula 34/aula34.py
+++ CursoPython-Udemy/aula 34/aula34.py
@@ -1,8 +1,15 @@
 """
 Funções (def) em Python - return - Aula 16 (parte 2)
 """
-def funcao(var):
-    print(var)
+def divisao(n1, n2):
+    if n2 == 0:
+        return
+    return n1/n2
 
-funcao('Valor que eu quero')
-print('OPA! consegui!')
+
+divide = divisao(8, 3)
+
+if divide:
+    print(f'{divide:.2f}') # se não houvesse um eslse, o resultado seria "NONE".
+else:
+    print('Conta inválida! Defina um número para n2 diferente de zero(0)')
