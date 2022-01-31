9c32e32f6b6189ab39c038b355aeb404ea16c889
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-01-31 18:23:19 -0300

    ex lesson 33 and 34 (Functions)

--- CursoPython-Udemy/aula 35/aula35.py
+++ CursoPython-Udemy/aula 35/aula35.py
@@ -13,8 +13,57 @@ valor do primeiro número somado do segundo do percentual do mesmo.
 """
 
 """
-4 -  Fizz Buzz - Se o parâmetro da função fo divisível por 2, retorn fizz. Se o parâmetro da função for divisível por 5,
+4 -  Fizz Buzz - Se o parâmetro da função fo divisível por 2, return fizz. Se o parâmetro da função for divisível por 5,
 retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número
 enviado.
 """
+######### exercício 1 #############
 
+def saudacao(saud='Olá!', name='Criatura!') -> str:
+    return f'{saud}! Como você está, {name}?'
+
+
+n1 = saudacao('Tudo bem?', 'JOSÉ')
+n2 = saudacao()
+print(n1)
+print(n2)
+
+######### exercício 2 #############
+
+def soma_3(n1, n2, n3):
+    return n1+n2+n3
+
+
+soma = soma_3(1, 3, 5)
+
+print(f'O resultado foi {soma}!!')
+
+######### exercício 3 #############
+
+def porcentagem(valor, percentual):
+    n_1 = valor
+    n_2 = (valor*percentual/100)
+    n_3 = n_1 + n_2
+    return print(f' {n_1} + {n_2} é igual a {n_3}.')
+
+
+porcentagem(10, 50)
+porcentagem(500, 20)
+porcentagem(100, 5)
+porcentagem(456, 125)
+
+######### exercício 4 #############
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FIZZBUZZ'
+    elif n % 5 == 0:
+        return 'BUZZ'
+    elif n % 3 == 0:
+        return 'FIZZ'
+    else:
+        return n
+
+print(fb(3))
+print(fb(5))
+print(fb(45))
