0f0ea01d675b26cf429c2de677da6c405d6b7b6c
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-01-31 17:05:56 -0300

    ex lesson 33 and 34 (Functions)

---
+++ CursoPython-Udemy/aula 35/aula35.py
@@ -0,0 +1,20 @@
+"""
+1 - Crie uma função que exibe uma saudação com os parâmetros saudação e nome.
+"""
+
+"""
+2- Crie uma função que recebe 3 números como parâmetros e exiba a soma entre eles.
+
+"""
+
+"""
+3 - Crie uma função que receba 2 números. O primeiro é um valor e o segundo um percentual (ex. 10%). Retorne (return) o
+valor do primeiro número somado do segundo do percentual do mesmo.
+"""
+
+"""
+4 -  Fizz Buzz - Se o parâmetro da função fo divisível por 2, retorn fizz. Se o parâmetro da função for divisível por 5,
+retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número
+enviado.
+"""
+
