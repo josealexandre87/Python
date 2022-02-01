794b346e6d244b3c150638cd80785efbb64cedfb
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-02-01 07:47:05 -0300

    ex36

---
+++ CursoPython-Udemy/aula 36/aula36.py
@@ -0,0 +1,10 @@
+"""
+Funções (def) em Python - *args **kwargs (parte 3)
+
+*arguments e **Keyword arguments
+"""
+
+def func(a1, a2, a3, a4, a5):
+    print(a1, a2, a3, a4, a5)
+
+func(1, 4, 5, 6, 7)
