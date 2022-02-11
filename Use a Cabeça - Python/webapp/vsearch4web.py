f3c69c7ce228ecde40ccdbe7b5b288824d3af9c3
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-02-11 11:03:22 -0300

    update search4web.py

--- Use a Cabeça - Python/webapp/vsearch4web.py
+++ Use a Cabeça - Python/webapp/vsearch4web.py
@@ -29,10 +29,15 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
-    with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+def view_the_log() -> 'html':
+        contents = []
+        with open('vsearch.log') as log:
+            for line in log:
+                contents.append([])
+                for item in line.split('|'):
+                    contents[-1].append(escape(item))
+        titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
 
 
 if __name__ == '__main__':
