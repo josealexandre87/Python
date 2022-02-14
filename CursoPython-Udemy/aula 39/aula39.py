6bd8381703e096bd80fc4b00b7c628a8fa255ec2
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-02-14 13:50:04 -0300

    39 - Expressões lambda (funções anônimas)

--- CursoPython-Udemy/aula 39/aula39.py
+++ CursoPython-Udemy/aula 39/aula39.py
@@ -2,3 +2,27 @@
 Expressões Lambda (funções anônimas) em Python
 """
 
+# def func(arg, arg2):
+#     return arg * arg2
+#
+# var = func(2,2)
+# print(var)
+#
+# a = lambda x, y: x * y
+# print(a(2,2))
+
+# def func(item):
+#     return item[1]
+
+lista = [
+    ['P1', 13],
+    ['P2', 6],
+    ['P3', 7],
+    ['P4', 50],
+    ['P5', 8],
+]
+
+# lista.sort(key=func, reverse=True)
+# print(lista)
+
+print(sorted(lista, key=lambda i: i[0], reverse=True)) #função anônima lambda. os parâmetros e return já estão nela.
