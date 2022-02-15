41f048c0a6957b8f27e0d88044efaf5547f72448
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-02-15 08:54:22 -0300

    43 - SETS

---
+++ CursoPython-Udemy/aula 43/aula43.py
@@ -0,0 +1,12 @@
+"""
+
+CONJUNTOS
+
+add (adiciona), upadate (atualiza), clear, discard
+union | (une)
+intersection & (todos os elementos presentes nos dois sets)
+diference - (elementos apenas no set da esquerda)
+symmetric_difference ^ (elementos que estão nos dois sets, mas não  em ambos)
+
+"""
+
