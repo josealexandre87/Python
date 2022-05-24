46d3434fd3515ef280b4ec1ddc4bdef28bef7790
josealexandre87 <82968453+josealexandre87@users.noreply.github.com>
2022-05-24 07:54:49 -0300

    aula 53 - cout() - itertools - (contador infinito)

---
+++ CursoPython-Udemy/aula 53/aula53.py
@@ -0,0 +1,27 @@
+"""
+
+Count - Intertools   (Contadores Infinitos)
+"""
+
+from itertools import count
+
+# contador = count(start=-5, step=1)
+#
+# for valor in contador:
+#     print(round(valor, 2))
+#     if valor >=10 or valor <= -10:
+#         break
+
+
+contador = count()
+
+lista = ['luiz', 'joão', 'Maria', 'josé', 'silva']
+lista = zip(contador, lista)
+print(list(lista))
+
+
+# print(next(contador))
+# print(next(contador))
+# print(next(contador))
+# print(next(contador))
+# print(next(contador))
